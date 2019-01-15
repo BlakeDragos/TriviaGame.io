7472f26ab40bdd9b14e563db7f02c304572f50f1
Blake Dragos <Mr.duncan8@gmail.com>
2019-01-14 21:31:26 -0600

    formated and tried picture change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,68 +15,68 @@ backgroundMusic.setAttribute("src", "assets/javascript/backgroundMusic.mp3");
 
 var Questions = [
     {
-        Question: "What is a critical success when rolled in Dungeons and Dragons?", 
-        answers: {a:"19", b:"20", c:"1"}, 
+        Question: "What is a critical success when rolled in Dungeons and Dragons?",
+        answers: { a: "19", b: "20", c: "1" },
         correctAnswer: "20",
         image: "assets/images/rolling.gif"
     },
     {
-        Question: "How many dice is 8d6?", 
-        answers: {a:"8", b:"14", c:"6"}, 
+        Question: "How many dice is 8d6?",
+        answers: { a: "8", b: "14", c: "6" },
         correctAnswer: "8",
         image: "assets/images/pileOfDice.jpg"
     },
     {
-        Question: "What is the multiplyer to damage that vulnerability gives?", 
-        answers: {a:"2x", b:"3x", c:"6x"}, 
+        Question: "What is the multiplyer to damage that vulnerability gives?",
+        answers: { a: "2x", b: "3x", c: "6x" },
         correctAnswer: "2x",
         image: "assets/images/vulnerability.png"
     },
     {
-        Question: "What is the name of the famous D&D game DM'd by Matthew Mercer? ", 
-        answers: {a:"Dynamic Dice", b:"Dice Camera Action", c:"Critical Role"}, 
+        Question: "What is the name of the famous D&D game DM'd by Matthew Mercer? ",
+        answers: { a: "Dynamic Dice", b: "Dice Camera Action", c: "Critical Role" },
         correctAnswer: "Critical Role",
         image: "assets/images/MatthewMercer.jpg"
     },
     {
-        Question: "What is the name of this creature?", 
-        answers: {a:"Godzilla", b:"Death Slaad", c:"Tarasque"}, 
+        Question: "What is the name of this creature?",
+        answers: { a: "Godzilla", b: "Death Slaad", c: "Tarasque" },
         correctAnswer: "Tarasque",
         image: "assets/images/Tarasque.jpeg"
     },
     {
-        Question: "What are the classes that cast arcane magic?", 
-        answers: {a:"Druic, Cleric, Paladin", b:"Wizard, Sorcerer, Bard, Warlock", c:"Barbarian, Ranger, Rogue"}, 
+        Question: "What are the classes that cast arcane magic?",
+        answers: { a: "Druic, Cleric, Paladin", b: "Wizard, Sorcerer, Bard, Warlock", c: "Barbarian, Ranger, Rogue" },
         correctAnswer: "Wizard, Sorcerer, Bard, Warlock",
         image: "assets/images/wizard.jpg"
     },
     {
-        Question: "How many dice are in a basic set of dice?", 
-        answers: {a:"7", b:"4", c:"10"}, 
+        Question: "How many dice are in a basic set of dice?",
+        answers: { a: "7", b: "4", c: "10" },
         correctAnswer: "7",
         image: "assets/images/baseDice.jpg"
     },
     {
-        Question: "What is the name of the clasic D&D magic item that holds items in an extra dimensional space?", 
-        answers: {a:"Bag of Holding", b:"Pocket of Tricks", c:"Spectacular Holder of Itmes"}, 
+        Question: "What is the name of the clasic D&D magic item that holds items in an extra dimensional space?",
+        answers: { a: "Bag of Holding", b: "Pocket of Tricks", c: "Spectacular Holder of Items" },
         correctAnswer: "Bag of Holding",
         image: "assets/images/bagOfHolding.jpg"
     },
     {
-        Question: "What version of D&D is curently in rotation?", 
-        answers: {a:"1st edition", b:"5th edition", c:"4th edition"}, 
+        Question: "What version of D&D is curently in rotation?",
+        answers: { a: "1st edition", b: "5th edition", c: "4th edition" },
         correctAnswer: "5th edition",
         image: "assets/images/d&d.png"
     },
     {
-        Question: "At what level do all major spellcasters get their 9th level spell slot?", 
-        answers: {a:"10", b:"20", c:"17"}, 
+        Question: "At what level do all major spellcasters get their 9th level spell slot?",
+        answers: { a: "10", b: "20", c: "17" },
         correctAnswer: "17",
         image: "assets/images/9thLevel.jpg"
     }
 
 ];
-function displayQuestion(){
+function displayQuestion() {
     $(".display-4").text(Questions[i].Question);
     $("#AnswersA").text(Questions[i].answers.a);
     $("#AnswersB").text(Questions[i].answers.b);
@@ -88,17 +88,17 @@ function nextQuestion() {
     stop();
     time = 30;
     $(".lead").text(time);
-    if (userGuess === Questions[i].correctAnswer){
-    cheer.play();
-    $("#image-holder").html("<img src='assets/images/correct.gif' width='200px'/>");
-    $(".display-4").text("Correct!!");
-    $("#AnswersA").text("");
-    $("#AnswersB").text("");
-    $("#AnswersC").text("");
-    rightAnswers++;
-    i++;
-    setTimeout(loop, 3000);
-    }else{
+    if (userGuess === Questions[i].correctAnswer) {
+        cheer.play();
+        $("#image-holder").html("<img src='assets/images/correct.gif' width='200px'/>");
+        $(".display-4").text("Correct!!");
+        $("#AnswersA").text("");
+        $("#AnswersB").text("");
+        $("#AnswersC").text("");
+        rightAnswers++;
+        i++;
+        setTimeout(loop, 3000);
+    } else {
         boo.play();
         $("#image-holder").html("<img src='assets/images/wrong.gif' width='200px'/>");
         $(".display-4").text("Wrong!");
@@ -112,7 +112,7 @@ function nextQuestion() {
 }
 
 
-function Reset(){
+function Reset() {
     $(".Start").show();
     $(".Restart").hide();
     $("#results").text("");
@@ -128,19 +128,19 @@ function Reset(){
 
 
 
-function checkfinish(){
-    if( i < tenQuestions ){
+function checkfinish() {
+    if (i < tenQuestions) {
         done = false;
         $("#image-holder").show();
         $(".Start").hide();
-    }else{
-    $(".Restart").show();
-    stop();
-    $("#image-holder").hide();
-    $(".display-4").text("Results!");
-    $(".lead").text("");
-    $("#results").text("you got "+ rightAnswers+" answers correct! and "+ WrongAnswers + " answers Wrong.");
-    done = true;
+    } else {
+        $(".Restart").show();
+        stop();
+        $("#image-holder").hide();
+        $(".display-4").text("Results!");
+        $(".lead").text("");
+        $("#results").text("you got " + rightAnswers + " answers correct! and " + WrongAnswers + " answers Wrong.");
+        done = true;
     }
 };
 
@@ -152,10 +152,10 @@ var clockRunning = false;
 function start() {
 
     if (!clockRunning) {
-      timer = setInterval(count, 1000);
-      clockRunning = true;
+        timer = setInterval(count, 1000);
+        clockRunning = true;
     }
-  }
+}
 
 function stop() {
 
@@ -166,12 +166,12 @@ function stop() {
 
 function count() {
 
-    time--;  
+    time--;
     $(".lead").text(time);
-    if(time === 0){
+    if (time === 0) {
         nextQuestion();
     }
-  }
+}
 
 
 function loop() {
@@ -179,11 +179,11 @@ function loop() {
     backgroundMusic.play();
     $(".lead").text(time);
     checkfinish()
-    if(!done)
-    displayQuestion();
-    
+    if (!done)
+        displayQuestion();
+
 }
-$(".Answer").click(function(){
+$(".Answer").click(function () {
     stop();
     time = 30;
     ding.play();
